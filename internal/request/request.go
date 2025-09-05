c711852610da4ba0f11d6be40c57b6a585bdf6b5
blakehulett7 <blake.hulett7@gmail.com>
2025-09-05 16:45:57 -0500

    checkpoint for the day... this is fun

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,10 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -13,5 +17,32 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	return &Request{}, nil
+	raw, err := io.ReadAll(reader)
+	if err != nil {
+		return &Request{}, err
+	}
+
+	parts := strings.Split(string(raw), "\r\n")
+	raw_request_line := parts[0]
+	request_line, err := parseRequestLine(raw_request_line)
+	if err != nil {
+		return &Request{}, err
+	}
+
+	return &Request{request_line}, nil
+}
+
+func parseRequestLine(line string) (RequestLine, error) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return RequestLine{}, fmt.Errorf("malformed request line")
+	}
+
+	method := parts[0]
+
+	return RequestLine{
+		HttpVersion:   parts[2],
+		RequestTarget: parts[1],
+		Method:        parts[0],
+	}, nil
 }
