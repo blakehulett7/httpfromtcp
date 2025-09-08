8307910c29b83db4a3685e451f86e841987eaef4
blakehulett7 <blake.hulett7@gmail.com>
2025-09-08 07:48:59 -0500

    not sure about this testing package

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"unicode"
 )
 
 type Request struct {
@@ -35,14 +36,34 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 func parseRequestLine(line string) (RequestLine, error) {
 	parts := strings.Split(line, " ")
 	if len(parts) != 3 {
-		return RequestLine{}, fmt.Errorf("malformed request line")
+		return RequestLine{}, fmt.Errorf("malformed request line: %v", line)
 	}
 
 	method := parts[0]
+	if !containsOnlyCapitalLetters(method) {
+		return RequestLine{}, fmt.Errorf("method must contain capital letters only: %v", method)
+	}
+
+	http_version_literal := parts[2]
+	version_parts := strings.Split(http_version_literal, "/")
+	http_version := version_parts[1]
+	if len(version_parts) != 2 || http_version != "1.1" {
+		return RequestLine{}, fmt.Errorf("malformed or incorrect http version: %v", http_version_literal)
+	}
 
 	return RequestLine{
-		HttpVersion:   parts[2],
+		HttpVersion:   http_version,
 		RequestTarget: parts[1],
-		Method:        parts[0],
+		Method:        method,
 	}, nil
+}
+
+func containsOnlyCapitalLetters(s string) bool {
+	for _, c := range s {
+		if !unicode.IsUpper(c) {
+			return false
+		}
+	}
+
+	return true
 }
